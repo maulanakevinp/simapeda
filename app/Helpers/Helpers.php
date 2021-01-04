438a6af62d14ac434493d65166d746511c4c15b3
Maulana Kevin Pradana <maulanakevinp@gmail.com>
2021-01-04 12:07:50 +0700

    update

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -29,18 +29,18 @@ if (! function_exists('bulan')) {
     function bulan($bulan)
     {
         $data = array (
-            1 =>   'Januari',
-            'Februari',
-            'Maret',
-            'April',
-            'Mei',
-            'Juni',
-            'Juli',
-            'Agustus',
-            'September',
-            'Oktober',
-            'November',
-            'Desember'
+            '01'    =>  'Januari',
+            '02'    =>  'Februari',
+            '03'    =>  'Maret',
+            '04'    =>  'April',
+            '05'    =>  'Mei',
+            '06'    =>  'Juni',
+            '07'    =>  'Juli',
+            '08'    =>  'Agustus',
+            '09'    =>  'September',
+            '10'    =>  'Oktober',
+            '11'    =>  'November',
+            '12'    =>  'Desember'
         );
         return $data[$bulan];
     }
